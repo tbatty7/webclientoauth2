9f5fe581a9c665cc5b347b052ff0bd34db0fd00c
Tim Batty <tbatty7@gmail.com>
2022-01-26 15:34:48 -0500

    Initial commit

---
+++ src/test/java/com/battybuilds/webclientoauth2/Webclientoauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battybuilds.webclientoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webclientoauth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
