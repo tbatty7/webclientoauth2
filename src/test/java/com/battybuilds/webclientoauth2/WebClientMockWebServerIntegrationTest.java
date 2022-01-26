dbad9dfe9dcbe176e3f9750845a60984587c9440
Tim Batty <tbatty7@gmail.com>
2022-01-26 16:05:56 -0500

    Added integration test

---
+++ src/test/java/com/battybuilds/webclientoauth2/WebClientMockWebServerIntegrationTest.java
@@ -0,0 +1,121 @@
+package com.battybuilds.webclientoauth2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.IOException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@SpringBootTest
+@TestPropertySource(properties = {"spring.main.allow-bean-definition-overriding=true"})
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("local")
+@ContextConfiguration(classes = {Webclientoauth2Application.class, WebClientMockWebServerIntegrationTest.TestConfig.class})
+class WebClientMockWebServerIntegrationTest {
+
+    public static MockWebServer mockServer;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    JwtDecoder jwtDecoder;
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        mockServer = new MockWebServer();
+        mockServer.start();
+    }
+
+    @AfterAll
+    static void afterAll() throws IOException {
+        mockServer.shutdown();
+    }
+
+    @Configuration
+    public static class TestConfig {
+        @Bean(name = "wokeWebClient")
+        WebClient dmppsWebClient() {
+            HttpUrl url = mockServer.url("/");
+            WebClient webClient = WebClient.create(url.toString());
+            return webClient;
+        }
+    }
+
+    @Test
+    void successResponse() throws Exception {
+        WokeResponse wokeResponse = WokeResponse.builder()
+                .alarm1("Time to get up")
+                .alarm2("You're gonna be late")
+                .alarm3("Your boss is calling")
+                .build();
+        String responseBody = objectMapper.writeValueAsString(wokeResponse);
+        mockBackendEndpoint(200, responseBody);
+
+        ResultActions resultActions = executeRequest("1234");
+
+        verify200Results(resultActions, 200, "\"alarm1\":\"Time to get up\"", "\"alarm2\":\"You're gonna be late\"");
+    }
+
+    @Test
+    void handles500ErrorsFromBackendServer() throws Exception {
+        WokeResponse wokeResponse = WokeResponse.builder().error("What does that even mean?").build();
+        String responseBody = objectMapper.writeValueAsString(wokeResponse);
+        mockBackendEndpoint(500, responseBody);
+
+        ResultActions resultActions = executeRequest("1234");
+
+        verify200Results(resultActions, 500, "\"error\":\"500 Internal Server Error", "context: WAKEUP");
+    }
+
+    private void mockBackendEndpoint(int responseCode, String body) {
+        MockResponse mockResponse = new MockResponse().setResponseCode(responseCode)
+                .setBody(body)
+                .addHeader("Content-Type", "application/json");
+        mockServer.enqueue(mockResponse);
+    }
+
+    private ResultActions executeRequest(String appId) throws Exception {
+        return mockMvc.perform(MockMvcRequestBuilders
+                .get("/v1/alarms")
+                .header("Identification-No", appId)
+                .header("Authorization", "Bearer 123"));
+    }
+
+    private void verify200Results(ResultActions resultActions, int status, String... message) throws Exception {
+        resultActions
+                .andDo(print())
+                .andExpect(status().is(status));
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        Assertions.assertThat(responseBody).contains(message);
+    }
+}
