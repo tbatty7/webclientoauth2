1affe0ca3dec9cc02d6069dc3726a50bed1826f3
Tim Batty <tbatty7@gmail.com>
2022-03-22 14:18:52 -0400

    Removed unnecessary annotations from integration tests and removed boilerplate test.

--- src/test/java/com/battybuilds/webclientoauth2/SecureWebClientIntegrationTest.java
+++ src/test/java/com/battybuilds/webclientoauth2/SecureWebClientIntegrationTest.java
@@ -13,10 +13,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,10 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @SpringBootTest
-@TestPropertySource(properties = {"spring.main.allow-bean-definition-overriding=true"})
 @AutoConfigureMockMvc(addFilters = false)
 //@ActiveProfiles("local")  Can be added if needed - still works
-@ContextConfiguration(classes = {WebClientOAuth2Application.class})
 class SecureWebClientIntegrationTest {
 
     public static MockWebServer mockAbcServer;
